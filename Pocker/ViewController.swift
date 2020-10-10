6686c26280af99bd565bc9f5f7f68d61323bd6b0
Lai Van Hai <vlai@netprotections.co.jp>
2020-10-10 18:56:07 +0900

    Init Project

---
+++ Pocker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pocker
+//
+//  Created by ライ ヴァン・ハイ on 2020/10/03.
+//  Copyright © 2020 ライ ヴァン・ハイ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PockerTests/PockerTests.swift
@@ -0,0 +1,34 @@
+//
+//  PockerTests.swift
+//  PockerTests
+//
+//  Created by ライ ヴァン・ハイ on 2020/10/03.
+//  Copyright © 2020 ライ ヴァン・ハイ. All rights reserved.
+//
+
+import XCTest
+@testable import Pocker
+
+class PockerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
